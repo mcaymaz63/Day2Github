3b90b6584d9d1a8676954f73acb492ed0ce74fd4
mevlut caymaz <mevlitkacmaz.63@gmail.com>
2022-08-15 21:40:53 +0300

    V3

--- src/Day2GitGithub.java
+++ src/Day2GitGithub.java
@@ -1,14 +1,11 @@
 
 
 
-
-
 /*
 1-) git init --> Local repo oluşturmak için yani .git ile klasörümüzün
 içindeki dosyaları ilişkilendirmek için kullanılır
 2-) git add . --> Working space'den (yani yerel) dosyalarımı staging area'ya
 (yani commitlemek için beklenen yer) gönderir
-3-) git status --> Working spacew   orking space
 3-) git commit -m "mesaj" --> Staging area'dan commit stora
 dosyalarımı göndermek için kullanılır (commit yani versiyon
 yani sürüm oluşturmuş olurum)
@@ -17,7 +14,7 @@ göndermek için kullandığımız kod Yalnız git push komutunu direk kullanmak
             git remote add origin adres
             git push -u origin master
 Not: Yukarıdaki iki komutu tek seferde kullandıktan sonra ikinci commit'lerim için sadece git push
-kullanırız
+kullanırıg --oneline commitlerimdeki yani versiyonlarimdaki durumu gosteirir
+
+NOT ; git lo9
  */
-
-
